f26ac340d0dba76284282171b97a1d9c25a77ae7
merdyIlunga <135390989+merdyIlunga@users.noreply.github.com>
2023-06-03 13:50:47 -0400

    Add files via upload
    mon site web d'un parc Aquatique version 1.0

---
+++ js/Script_formulaire.js
@@ -0,0 +1,6 @@
+
+function changer_formulaire()
+{
+    document.getElementById("formulaire_billet1").style.display = "none";
+    document.getElementById("formulaire_billet2").style.display = "block";
+}
